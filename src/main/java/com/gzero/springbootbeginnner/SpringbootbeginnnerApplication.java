4a7f93d37f44a41d9cab75e770851193dafdcc89
jyhanMacM1 <jiyounghan@gmail.com>
2023-01-07 16:49:52 +0900

    Initial commit

---
+++ src/main/java/com/gzero/springbootbeginnner/SpringbootbeginnnerApplication.java
@@ -0,0 +1,13 @@
+package com.gzero.springbootbeginnner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootbeginnnerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootbeginnnerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gzero/springbootbeginnner/SpringbootbeginnnerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gzero.springbootbeginnner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootbeginnnerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
